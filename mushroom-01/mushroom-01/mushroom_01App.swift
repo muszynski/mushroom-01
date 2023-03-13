71cb3da29ef8bad9a93953249a439fd8f414531c
muszynski <muszynski@gmail.com>
2023-03-13 20:51:18 +0100

    init

---
+++ mushroom-01/mushroom-01/mushroom_01App.swift
@@ -0,0 +1,17 @@
+//
+//  mushroom_01App.swift
+//  mushroom-01
+//
+//  Created by Łukasz Muszyński on 13/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct mushroom_01App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
